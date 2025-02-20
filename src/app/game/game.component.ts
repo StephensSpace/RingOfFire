3e8f35a3cd41349bf29c346f545b17dd69bc4102
Stephen Schaz <stephenschaz90@gmail.com>
2025-02-20 12:12:00 +0100

    Players added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerbarComponent } from '../playerbar/playerbar.component';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule, PlayerbarComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
 
+
+export class GameComponent {
+  pickCardAnimation: boolean = false;
+  currentCard: string | undefined
+  game?: Game 
+
+  constructor() {
+    this.newGame();
+    console.log(this.game)
+    this.currentCard = '';
+  }
+
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  PickCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop();
+      if (this.currentCard !== undefined) {
+        this.game?.playedCard.push(this.currentCard);
+      }
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  
+  }
 }
---
+++ src/app/playerbar/playerbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-playerbar',
+  imports: [],
+  templateUrl: './playerbar.component.html',
+  styleUrl: './playerbar.component.scss'
+})
+export class PlayerbarComponent {
+  
+  @Input() name: string = 'Player';
+
+}
