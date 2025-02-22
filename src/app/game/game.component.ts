8cb39b55a9464b0de6155c0f5f5da0854ec2f133
Stephen Schaz <stephenschaz90@gmail.com>
2025-02-22 15:30:46 +0100

    Player changes uppon stack click

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,22 +3,22 @@ import { Component, signal, model, inject } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerbarComponent } from '../playerbar/playerbar.component';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
 
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule, 
-  PlayerbarComponent, MatIconModule, 
-  MatButtonModule, MatInputModule, 
-  MatFormFieldModule, FormsModule,
-  DialogComponent ],
+  imports: [CommonModule,
+    PlayerbarComponent, MatIconModule,
+    MatButtonModule, MatInputModule,
+    MatFormFieldModule, FormsModule,
+    DialogComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -30,8 +30,8 @@ export class GameComponent {
   readonly dialog = inject(MatDialog);
   pickCardAnimation: boolean = false;
   currentCard: string | undefined
-  game?: Game 
-  
+  game?: Game
+
 
   constructor() {
     this.newGame();
@@ -49,6 +49,13 @@ export class GameComponent {
       if (this.currentCard !== undefined) {
         this.game?.playedCard.push(this.currentCard);
       }
+      if (this.game?.currentPlayer !== undefined) {
+        if (this.game?.currentPlayer < this.game?.players.length -1) { 
+          this.game.currentPlayer++; 
+        } else {
+          this.game.currentPlayer = 0;
+        }
+      }
       this.pickCardAnimation = true;
       setTimeout(() => {
         this.pickCardAnimation = false;
@@ -58,14 +65,14 @@ export class GameComponent {
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogComponent, {
-      data: { animal: ''},
+      data: { animal: '' },
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result !== undefined) {
         this.animal.set(result);
-        this.game?.players.push( this.animal());
-      } 
+        this.game?.players.push(this.animal());
+      }
     });
   }
 
